d4ecf6914c81df2e27d71e9f99f121fcb28e6b7b
Federico Pantaleone <federico.pantaleone@globant.com>
2025-01-27 14:39:23 -0300

    removed the only-modifier from tests

--- cypress/e2e/interceptMockedApiTest.spec.js
+++ cypress/e2e/interceptMockedApiTest.spec.js
@@ -29,7 +29,7 @@ describe("Intercept and Mocked API Tests", ()=>{
         cy.contains("button", "Delete Article").click()
     })
 
-    it.only("Verify tags are shown correctly", ()=>{
+    it("Verify tags are shown correctly", ()=>{
         let expectedTags
         cy.intercept("GET", apiTagsUrl).as("tags")
         
